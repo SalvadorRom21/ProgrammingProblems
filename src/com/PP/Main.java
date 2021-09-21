0297915e26e270a889fa915fb8393bf7c5c2cbcb
romer <romero.salvador.am@gmail.com>
2021-09-21 18:10:56 -0500

    Adding MultiplesOf3And5.java

--- src/com/PP/Main.java
+++ src/com/PP/Main.java
@@ -1,16 +1,16 @@
 package com.PP;
 
 import com.PP.easy.*;
+import com.PP.ProjectEuler.*;
 
 
 import java.util.Arrays;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        String[] strings = {"flower","flower","flower","flower"};
-        System.out.println(LongestCommonPrefix.LongestPrefix(strings));
+        int num = MultiplesOf3And5.sumOfMultiples(1000);
+        System.out.println(num);
     }
-
-
 }
---
+++ src/com/PP/ProjectEuler/MultiplesOf3And5.java
@@ -0,0 +1,27 @@
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+package com.PP.ProjectEuler;
+
+public class MultiplesOf3And5 {
+
+
+    public static int sumOfMultiples(int maxNum){
+        int sum = 0;
+        for(int i = 1; i < maxNum;i++){
+            if(i%3==0 || i%5 ==0){
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
+
+/*
+Solution to solve this problem we will create a for loop that will check if the number is a multiple of 3 or 5.
+We do this by checking if the number that is being produced by a mod is 0 for either 3 or 5.
+If the number meets either of those requirements then we will add it to the sum and continue to the next number.
+ */
