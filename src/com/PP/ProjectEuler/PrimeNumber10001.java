b0cae6c5749b35e124eab264e7f257cf14ab3808
romer <romero.salvador.am@gmail.com>
2021-09-27 01:37:16 -0500

    Dreamworld Interview

--- src/com/PP/ProjectEuler/PrimeNumber10001.java
+++ src/com/PP/ProjectEuler/PrimeNumber10001.java
@@ -1,16 +1,34 @@
 package com.PP.ProjectEuler;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class PrimeNumber10001 {
+
+
     public static void primeNum10001() {
 
-        LinkedList<Long> primes = new LinkedList<>();
+        int count = 0;
+        outerloop:
+        for(int n = 3; n < Integer.MAX_VALUE; n++) {
+            boolean isPrime = true;
+            int i = 2;
+            while (i <= n / 2) {
 
-        for(Long i = 0L; i < Integer.MAX_VALUE; i++){
-            primes.add(i);
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+                i++;
+            }
+            if(isPrime){
+                count++;
+                if(count == 10000){
+                    System.out.println(n);
+                    break outerloop;
+                }
+            }
         }
-        System.out.println(primes);
-
     }
 }
